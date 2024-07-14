f634056bb401d971f081dc3133493b7f658385de
m6io <m5dk4r@gmail.com>
2024-07-14 19:50:47 -0400

    chore: begin reducing bundle size, package version bumped

--- packages/json-schema/vite.config.ts
+++ packages/json-schema/vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   build: {
@@ -10,13 +11,30 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react"],
+      external: [
+        "react",
+        // "ajv", //todo: decouple
+        // "ajv-formats",
+        // "zustand",
+        // "json-schema",
+      ],
       output: {
         globals: {
           react: "React",
+          // ajv: "Ajv", //todo: decouple
+          // "ajv-formats": "AjvFormats",
+          // zustand: "Zustand",
+          // "json-schema": "JsonSchema",
         },
       },
     },
+    minify: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "dist/stats.html",
+      open: true,
+    }),
+  ],
 });
